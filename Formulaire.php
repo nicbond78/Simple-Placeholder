a80b1b609c3157732a7963983d41d9670d66a51e
nicbond <nicolasmartins78650@free.fr>
2016-11-08 12:11:39 +0100

    Récupération et affichage du prénom dans Formulaire.php

--- Formulaire.php
+++ Formulaire.php
@@ -1,6 +1,6 @@
 <?php
-if (isset($_POST["nom"]) && isset($_POST["niveau"]))
+if (isset($_POST["nom"]) && isset($_POST["prenom"]) && isset($_POST["niveau"]))
 {
-	echo "<h2> Bonjour M ".htmlentities($_POST["nom"]). ", vous etes ".$_POST["niveau"]. " en PHP 5</h2>";
+	echo "<h2> Bonjour M ".htmlentities($_POST["nom"]). " ".htmlentities($_POST["prenom"]). ", vous etes ".$_POST["niveau"]. " en PHP 5</h2>";
 }
 ?>
